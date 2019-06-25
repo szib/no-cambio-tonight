e5ee10f2f912bdf2212d741511cf53b541740862
Ivan Szebenszki <szib77@gmail.com>
2019-06-25 15:17:44 +0100

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Route exact path="/events" render={() => <div>Events</div>} />
+      <Route path="/events/:id" render={(props) => <div>Event: {props.match.params.id}</div>} />
+      <Route path="/games" render={() => <div>Games</div>} />
+      <Route exact path="/" render={() => <div>Root</div>} />
+    </Router>
   );
 }
 
