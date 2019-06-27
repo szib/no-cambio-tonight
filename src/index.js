ca5670f6c4fd3ff97c4f0682cd3d10376380b4be
Ivan Szebenszki <szib77@gmail.com>
2019-06-27 20:20:37 +0100

    Refactor redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers';
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import store from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
