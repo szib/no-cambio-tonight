ac61c2797b1021397ec17a77477c62a57aa7d10e
Ivan Szebenszki <szib77@gmail.com>
2019-06-27 21:01:18 +0100

    Connect redux to profilepage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore } from 'redux';
 
-import reducer from './reducers';
+// import reducer from './reducers';
+import reducer from './reducers/index';
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+console.log('reducer', reducer)
 
 export default store
