c1730c803f53052b330400be77d6c496ff71f033
Ivan Szebenszki <szib77@gmail.com>
2019-07-06 13:29:12 +0100

    Add UserPage

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -5,28 +5,28 @@ import { Header, List, Grid, Image } from 'semantic-ui-react';
 const UserInfo = ({ user }) => {
   const {
     fullName,
-    username,
     memberSince,
     email,
     picture,
     numberOfOrganisedEvents,
     numberOfAttendedEvents
   } = user;
-  const { medium, large } = picture;
 
   return (
     <Grid>
       <Grid.Column width={2}>
-        <Image src={large} bordered circular size="big" />
+        <Image src={picture.large} bordered circular size="big" />
       </Grid.Column>
 
       <Grid.Column width={14}>
         <Header as="h1">{fullName}</Header>
         <List>
-          <List.Item>
-            <List.Icon name="mail" />
-            <List.Content>{email}</List.Content>
-          </List.Item>
+          {email && (
+            <List.Item>
+              <List.Icon name="mail" />
+              <List.Content>{email}</List.Content>
+            </List.Item>
+          )}
           <List.Item>
             <List.Icon name="clock outline" />
             <List.Content>Joined: {memberSince}</List.Content>
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import useAuthentication from '../hooks/useAuthentication';
+import useAPI from '../hooks/useAPI';
+
+import { Container, Segment } from 'semantic-ui-react';
+
+import Loader from '../components/LoaderWithDimmer';
+import UserInfo from '../components/UserInfo';
+
+const ProfilePage = ({ match }) => {
+  useAuthentication();
+  const id = match.params.id;
+  const userAPI = useAPI(`http://localhost:3030/api/v1/users/${id}`, {});
+
+  return (
+    <Container>
+      <Segment>
+        {userAPI.loading || !userAPI.data.user ? (
+          <Loader content="Loading profile..." />
+        ) : (
+          <UserInfo user={userAPI.data.user} />
+        )}
+      </Segment>
+    </Container>
+  );
+};
+
+export default ProfilePage;
