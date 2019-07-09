36f7347f33ea600d744f9900091d8871ebd8e449
Ivan Szebenszki <szib77@gmail.com>
2019-07-09 11:15:26 +0100

    Add new comment form

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Segment, Item } from 'semantic-ui-react';
 
 import Comment from './Comment';
+import NewCommentForm from './NewCommentForm';
 
 const Comments = ({ comments }) => {
   if (!comments) return null;
 
+  const submitHandler = text => {
+    console.log(text);
+  };
+
   return (
     <Segment>
+      <NewCommentForm onSubmitHandler={submitHandler} />
       <Item.Group divided>
         {comments &&
           comments.map(comment => (
