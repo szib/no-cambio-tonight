08fec49ed1151bafc3e141a19ef25022598c8cad
Ivan Szebenszki <szib77@gmail.com>
2019-07-10 14:50:04 +0100

    Positive/negative based on past/future events

--- src/pages/EventsPage/EventRow.jsx
+++ src/pages/EventsPage/EventRow.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import { DateTime } from 'luxon';
 import { Table } from 'semantic-ui-react';
 
 import EventLabels from '../../components/EventLabels';
 import EventDateTime from '../../components/EventDateTime';
 
 const EventRow = ({ event, history }) => {
+  const diffFromNow = DateTime.fromISO(event.startDateTime).diffNow().values;
+  const pastEvent = diffFromNow && diffFromNow.milliseconds > 0;
+
   return (
-    <Table.Row onClick={() => history.push(`/events/${event.id}`)}>
+    <Table.Row
+      onClick={() => history.push(`/events/${event.id}`)}
+      positive={pastEvent}
+      negative={!pastEvent}
+    >
       <Table.Cell>
         {event.title}
         <EventLabels event={event} />
