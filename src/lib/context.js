4056408e76334823141d5ad6b705a456c4e7be5e
Ivan Szebenszki <szib77@gmail.com>
2019-08-01 12:14:24 +0200

    Add contexts

---
+++ src/lib/context.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const AuthenticationContext = React.createContext({
+  status: 'PENDING',
+  token: '',
+  error: ''
+});
+
+export const MyGamesContext = React.createContext({
+  gamePieces: [],
+  loading: false,
+  error: null
+});
+
+export const UserContext = React.createContext({
+  username: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  memberSince: '',
+  picture: {},
+  gender: 0
+});
